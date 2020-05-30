4f102de669864faff1806c6cff201fc19681c279
WayneLi1063 <weileli@uw.edu>
2020-05-29 22:29:01 -0700

    Change from fragment to activity

--- app/src/main/java/com/example/virtualstudygroup/MainActivity.kt
+++ app/src/main/java/com/example/virtualstudygroup/MainActivity.kt
@@ -2,33 +2,56 @@ package com.example.virtualstudygroup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), OnGroupClickedListener {
+class MainActivity : AppCompatActivity() {
 
     companion object {
         const val TAG = "Printing"
     }
 
+    private var groupListAdapter: GroupListAdapter? = null
+    private lateinit var groupsList: MutableList<Group>
+    private val database = Firebase.database
+    private val groups = database.getReference("groups")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        groupListAdapter = GroupListAdapter(mutableListOf<Group>())
+        rvGroupList.adapter = groupListAdapter
 
-        if (getGroupListFragment() == null) {
-            val groupListFragment = GroupListFragment.getInstance()
-            supportFragmentManager
-                .beginTransaction()
-                .add(
-                    R.id.fragContainer, groupListFragment, GroupListFragment.TAG
-                )
-                .commit()
+        groupListAdapter?.onGroupClickListener = { group ->
+            onGroupClicked(group)
         }
 
+        groups.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                groupsList = dataSnapshot.getValue<MutableMap<String, Group>>()?.values?.toMutableList()!!
+                groupListAdapter?.updateGroup(groupsList)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.i(TAG, "Failed to read value.", error.toException())
+            }
+        })
     }
 
-    private fun getGroupListFragment() =
-        supportFragmentManager.findFragmentByTag(GroupListFragment.TAG) as? GroupListFragment
+//    private fun getGroupListFragment() =
+//        supportFragmentManager.findFragmentByTag(GroupListFragment.TAG) as? GroupListFragment
 
-    override fun onGroupClicked(group: Group) {
+    private fun onGroupClicked(group: Group) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
