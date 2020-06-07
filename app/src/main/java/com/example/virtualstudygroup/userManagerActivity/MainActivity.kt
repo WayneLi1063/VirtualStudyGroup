1ab1f0f9011de549313c09980549e90c789d9ea2
xuan-yang0506 <xuany2@cs.washington.edu>
2020-06-07 13:26:20 -0700

    user can stay logged in when app is closed

--- app/src/main/java/com/example/virtualstudygroup/userManagerActivity/MainActivity.kt
+++ app/src/main/java/com/example/virtualstudygroup/userManagerActivity/MainActivity.kt
@@ -6,6 +6,10 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.virtualstudygroup.R
+import com.example.virtualstudygroup.chatActivity.MessageActivity
+import com.example.virtualstudygroup.getApp
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,12 +19,38 @@ class MainActivity : AppCompatActivity() {
 
         supportActionBar?.hide()
 
-        Toast.makeText(this, getString(R.string.start_message), Toast.LENGTH_SHORT).show()
-
         val layout: ConstraintLayout = findViewById(R.id.welcome_screen)
-        layout.setOnClickListener {
-            val intent: Intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
+
+
+        Firebase.auth.addAuthStateListener {
+            if (it.currentUser == null) {
+                layout.setOnClickListener {
+                    val intent: Intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                }
+                alertUser(false)
+            } else {
+                getApp().currentUser = it.currentUser
+                layout.setOnClickListener {
+                    val intent = Intent(this, MessageActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    startActivity(intent)
+                }
+                alertUser(true)
+            }
+        }
+
+    }
+
+    private fun alertUser(isSignedIn: Boolean) {
+        if (!isSignedIn) {
+            Toast.makeText(this, getString(R.string.start_message),
+                Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, getString(R.string.user_already_signedin_message)
+                                            .format(getApp().currentUser!!.email),
+                Toast.LENGTH_SHORT).show()
+
         }
     }
 }
